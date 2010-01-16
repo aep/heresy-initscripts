e1134c76399225c9f3cb51b3aa394e2d1cdc01fb
Lucas de Vries <lucas@glacicle.org>
2010-01-16 23:10:43 +0100

    create rc.d correctly

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ install -Dm755 init.conf.default ${DESTDIR}/etc/init.conf
 cp inittab ${DESTDIR}/etc
 
 # Initscripts
-install -Dm755 filesystem ${DESTDIR}/usr/rc.d
-install -Dm755 udev ${DESTDIR}/usr/rc.d
+install -Dm755 filesystem ${DESTDIR}/usr/rc.d/filesystem
+install -Dm755 udev ${DESTDIR}/usr/rc.d/udev
 cp functions initfunctions ${DESTDIR}/usr/rc.d
